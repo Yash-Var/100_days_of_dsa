f9bc6b78e487813237039d8b58220f53a28971e2
Yash Varshney <yash.2125cs1155@kiet.edu>
2022-09-26 18:50:45 +0530

    circular linked insertion at the beggning

---
+++ Linked_List/insert_at_begning_of_circular_linked_list.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+Node *insert(Node *head, int x)
+{
+    Node *temp = new Node(x);
+    if (head == NULL)
+        temp->next = temp;
+    else
+    {
+
+        Node *cur = head;
+        while (cur->next != head)
+        {
+            cur = cur->next;
+        }
+        cur->next = temp;
+        temp->next = head;
+    }
+    return temp;
+}
+void output(Node *head)
+{
+    if (head == NULL)
+        return;
+
+    Node *cur = head;
+    do{
+        cout<<cur->data<<" ";
+        cur=cur->next;
+
+    }while(cur!=head);
+}
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = head;
+    output(head);
+    cout<<endl;
+    head = insert(head, 5);
+   
+    output(head);
+}
